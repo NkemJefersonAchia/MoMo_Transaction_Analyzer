4c709a89479ea54ba4ea8711aba34996a51cee2d
NkemJefersonAchia <nkemachia05@gmail.com>
2026-01-21 22:45:02 +0100

    rest api

--- api/rest_api.py
+++ api/rest_api.py
@@ -23,3 +23,4 @@ class APIHandler(BaseHTTPRequestHandler):
             self.send_response(200)
             self.send_header('Content-Type', 'application/json')
             self.end_headers()
+            self.wfile.write(json.dumps(transactions).encode)
